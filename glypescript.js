f7dca2da9233c3498169eb634848f35279f86e3d
yakov <yakov@ginkgobioworks.com>
2013-08-29 17:00:06 -0400

    encoding url submitted to glype

--- glypescript.js
+++ glypescript.js
@@ -1,6 +1,6 @@
 chrome.browserAction.onClicked.addListener(function() {
 	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-		var tabUrl = tabs[0].url;
+		var tabUrl = encodeURIComponent(tabs[0].url);
 		var newTab = true;
 		var glypeUrl = "http://albertyw.mit.edu/codes/glype/browse.php?u="+tabUrl;
 		if (newTab) {
