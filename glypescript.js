ef3e98b96f00bec25fb141d0105a3f57535f1ee3
Albert Wang <albert@ginkgobioworks.com>
2013-08-29 17:52:14 +0000

    Changed name to glypifier and simplified glypescript.js

--- glypescript.js
+++ glypescript.js
@@ -1,20 +1,12 @@
 chrome.browserAction.onClicked.addListener(function() {
 	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
 		var tabUrl = tabs[0].url;
-		var newTab = true;
+		var newTab = false;
+		var glypeUrl = "http://albertyw.mit.edu/codes/glype/browse.php?u="+tabUrl;
 		if (newTab) {
-			chrome.tabs.create({'url': "http://albertyw.mit.edu/codes/glype/"}, function(tab) {
-				chrome.tabs.executeScript(tab.id, {
-					code: "document.getElementById('input').value='"+tabUrl+"'; document.forms[0].submit();"
-				});
-			});
-		}
-		else { // does not work
-			chrome.tabs.update(tabs[0].id, {'url': "http://albertyw.mit.edu/codes/glype/"}, function(tab) {
-				chrome.tabs.executeScript(tab.id, {
-					code: "document.getElementById('input').value='"+tabUrl+"'; document.forms[0].submit();"
-				});
-			});
+			chrome.tabs.create({'url': glypeUrl});
+		} else {
+			chrome.tabs.update(tabs[0].id, {'url': glypeUrl});
 		}
 	});
 });
